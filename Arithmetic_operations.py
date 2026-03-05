42df44a5d8e4f57ce458233f123d8115ad955133
Vijaya Mohan <vijayakalaiselvan236@gmail.com>
2026-03-05 08:04:13 +0000

    Add floor division operation and update output

--- Arithmetic_operations.py
+++ Arithmetic_operations.py
@@ -10,6 +10,7 @@ multiplication = num1 * num2
 # Division-related operations (handle divide-by-zero safely)
 if num2 != 0:
     division = num1 / num2
+    floor_division = num1//num2
 else:
     print("Division (num1 / num2): Not possible (cannot divide by zero)")
 
@@ -23,5 +24,6 @@ print(f"Multiplication (num1 * num2): {multiplication}")
 
 if num2 != 0:
     print(f"Division (num1 / num2): {division}")
+    print(f"Floor Division (num1 // num2): {floor_division}")
 
 print(f"Power (num1 ** num2): {power}")
